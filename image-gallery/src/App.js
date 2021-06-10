4591dcac48be925a7b574c36065acf87dd71a35d
dmoreira317 <dmoreiraventas@gmail.com>
2021-06-10 15:42:31 -0300

    added tailwind styles

--- image-gallery/src/App.js
+++ image-gallery/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./assets/css/styles.css";
 
-function App() {
+//Class based component, less used since the introduction of hooks, and functional components
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props); //im bringing the props from the React.Component class
+//     this.state = { title: "Hello React 2" };
+//   }
+//   render() {
+//     return <div>{this.state.title}</div>;
+//   }
+// }
+
+// just for rendering or static features
+//double curly braces for accessing the object props for style for instance, one for the prop and another for the style object
+function App({ title }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="bg-gray-600 text-white p-5 border">
+        {title}, Hola con estilo
+      </div>
     </div>
   );
 }
-
 export default App;
