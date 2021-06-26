9dad815b5b782f5e7726f9e884d8578848159994
Diego M <77156529+dmoreira317@users.noreply.github.com>
2021-06-25 23:17:34 -0300

    FIRST COMMIT AFTER CLEANING CODE

--- image-gallery/src/App.js
+++ image-gallery/src/App.js
@@ -2,100 +2,8 @@ import React, { useState, useEffect, useRef } from "react";
 import "./assets/css/styles.css";
 import Images from "./components/Images";
 
-//!Class based component, less used since the introduction of hooks, and functional components
-// class App extends React.Component {
-//   constructor(props) {
-//     super(props); //im bringing the props from the React.Component class
-//     this.state = { title: "Hello React", isShowing: false };
-//     console.log("App constructor");
-//   }
-//   //states are immutable, you need to use a setState to update a state
-
-//   handleClick = () => {
-//     this.setState({ isShowing: !this.state.isShowing });
-//     // this.state is an object thus i need to make the update as an object
-
-//     //    This below also works, but its not as short and pretty
-//     //   if (this.state.isShowing == false) {
-//     //     return this.setState({ isShowing: true });
-//     //   } else {
-//     //     return this.setState({ isShowing: false });
-//     //   }
-//   };
-
-//   //This hook means that the app is fully mounted, i can also check when an app is constructed, and also rendered and how it changes by consoleloggin it, this below will only work after the component is ready.
-//   componentDidMount() {
-//     console.log("App Mounted");
-//     this.setState({
-//       title: " title state was changed to: Roses image on and off",
-//     });
-//   }
-
-//   //This hook runs only when an update happens
-//   componentDidUpdate() {
-//     console.log("App updated");
-//   }
-
-//   render() {
-//     console.log("App render");
-//     return (
-//       <section className="flex justify-center">
-//         <div className="w-1/2">
-//           <div className="text-center">
-//             <div className="my-4">{this.state.title}</div>
-//             <div>
-//               <button
-//                 className="p-1 bg-blue-700 text-white mb-2"
-//                 onClick={this.handleClick}
-//               >
-//                 Toggle Image
-//               </button>
-//             </div>
-//             {this.state.isShowing ? <Images /> : null}
-//           </div>
-//         </div>
-//       </section>
-//     );
-//   }
-// }
-//! end of class based component ------------------------
-
-//TODO functional componentsjust for rendering or static features
-//double curly braces for accessing the object props for style for instance, one for the prop and another for the style object
-
 function App() {
   const [title, setTitle] = useState("Hello react");
-
-  //?This states are related to the toggle button, handle change and effects, which were meant to show the lifecycle of components
-  //   const [isShowing, setIsShowing] = useState(false);
-  //   const [didMount, setdidMount] = useState(false);
-  //   const mountRef = useRef(false);
-
-  // A function to show/hide the images, works along the button in return.
-  //   function handleClick() {
-  //     setIsShowing(!isShowing);
-  //   }
-
-  //All these effects were created alogn with toggle button and handle click to show the components lifecycle
-  //   useEffect(() => {
-  //     //Triggered on component mounted, by not using the second argument [],
-  //     setdidMount(true);
-  //     console.log("App mounted");
-  //     setTitle("Title changed by setTitle");
-  //     return () => {};
-  //   }, []);
-
-  //   //Component will update
-  //   useEffect(() => {
-  //     //Triggered on component updated, by using the brackets, if the state is passed in those brackets, this useEffect only works for that updated state. Here i check the state of the mounting, which is changed only when app is mounted and only then i give the log for the update below
-  //     if (mountRef.current) {
-  //       console.log("App updated");
-  //     } else {
-  //       mountRef.current = true;
-  //     }
-  //     return () => {};
-  //   }, [isShowing]);
-  //? end of toggle button states----------------------------------
 
   return (
     <section className="flex justify-center">
@@ -105,19 +13,7 @@ function App() {
           <div className="my-4">{title}</div>
 
           <Images />
-          {/* // only to use for example, this activates or deactivates the images on-screen
-          <div>
-             <button
-              className="p-1 bg-blue-700 text-white mb-2"
-              onClick={handleClick}
-            >
-              Toggle Image
-            </button>
-          </div>
-           
-          {isShowing ? <Images /> : null}
-           
-          </div>*/}
+     
         </div>
       </div>
     </section>
--- image-gallery/src/components/Images.js
+++ image-gallery/src/components/Images.js
@@ -8,121 +8,35 @@ import Image from "./image";
 
 //?functional component
 export default function images() {
-  // const [images, setimages] = useState([]); //!imported via fetchImages hook, right below here
-
   const [images, setimages] = useFetchImage();
 
-  //I'll import the hook useScroll here, generally, hoooks start with useXxxx names.
   const scrollPosition = useScroll();
 
-  //TODO more useffect tests on image array and input focus ------------
-  //This inputref is to hold focus on an input box only
   const inputref = useRef(null);
 
-  //to be used as reference for state changes of images (when deleting for instance)
   const varRef = useRef(0);
 
-  //useffect for mount of inputref
   useEffect(() => {
     inputref.current.focus();
-    
-    //TODO Ill use this below as a hook, useFetchImage
-    // axios
-    //   .get(
-    //     `${process.env.REACT_APP_UNSPLASH_URL}?client_id=${process.env.REACT_APP_UNSPLASH_KEY}`
-    //   )
-    //   .then((res) => {
-    //     setimages(res.data);
-    //     //console.log(res);
-    //   });
-    //this below is using JS only, above we use the useRef mode.
-    // const inputBox = document.getElementById("inputBox");
-    // inputBox.focus();
-    //console.log(varRef);
-    //TODO
+ 
   }, []);
 
-  //useffect to keep track of how many updates are made on my images array (just for updates)
   useEffect(() => {
     varRef.current = varRef.current + 1;
-    //setting an update within an update useEffect, will create an infinite loop, because once i setUpdate it actually is an update, so it does the useeffect again and so on. That's why we use useRef, to hold a state, which behaves like a varibale instead of a state.
-    //setupdateCount(updateCount + 1);
+   
     return () => {};
-  }, [varRef.current]); //this makes the useeffect run on every render, if i give it empty, it runs only once. (if this prop changes, it triggers again)
-  //TODO end of useeffect for arrays images----------------------------
-
-  //? testing the uselayouteffect and useeffect -----------------------
-  //   const [myName, setmyName] = useState("Diego");
-
-  //   // use effect runs after the new rendering is complete, the component is already printed on screen
-  //   useEffect(() => {
-  //     console.log("i am useeffect 1");
-  //   });
-
-  //   //This layout effect runs along with the re-render, synchronously, used for rare cases.
-  //   useLayoutEffect(() => {
-  //     setmyName("my new name is React JS");
-  //     console.log("i am useLayouteffect 2");
-  //   });
-  //? end of layout test -----------------------------------------------
-
-  //* Image states, remove handler -------------------------------------
-  const [newImageUrl, setnewImageUrl] = useState(""); //This state will be reflected in the input where i have the input (and is realted to this state, in the return section)
+  }, [varRef.current]);
+  const [newImageUrl, setnewImageUrl] = useState(""); 
 
   function handleRemove(index) {
-    //console.log(`image index: ${index}`);
-
-    //this filter removes whatever we filter, in this case, anything different from current image
-    //console.log(images.filter((image, i) => i !== index));
-    //setimages(images.filter((image, i) => i !== index));
-
-    //fitering with spread operator, which opens the array into elements, the brackets here create a new array, with the separated elements of the spread operator.
-    //A good example for spread operator is a book, and you want to separate the pages, so you get rid of the back and fron tcover, which is what spread does, with the array and gets elements only. Therefore i add brackets to create a new book.
-    // console.log([
-    //   ...images.slice(0, index),
-    //   ...images.slice(index + 1, images.length),
-    // ]);
-
+   
     setimages([
       ...images.slice(0, index),
       ...images.slice(index + 1, images.length),
     ]);
-    //console.log("i am changing the state");
+
   }
-  //* Image states, remove handler----------------------------
-
-  //! I dont need this variable below, i just use the state as a holder for the first images
-  //   const images = [
-  //     "https://images.unsplash.com/photo-1623275563425-82bfaf4599a3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
-
-  //     "https://images.unsplash.com/photo-1593642634402-b0eb5e2eebc9?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
-
-  //     "https://images.unsplash.com/photo-1623340437564-0f27a51621bc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80",
-
-  //     "https://images.unsplash.com/photo-1623334689744-ffe1e9fa9137?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1252&q=80",
-
-  //     "https://images.unsplash.com/photo-1623334577342-d65961791db8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
-  //   ];
-
-  //This effects were made to show mounting and unmounting a component, as its lifecycle
-  //   useEffect(() => {
-  //     console.log("Images comp mounted");
-
-  //     const interval = setInterval(() => {
-  //       console.log("Hello");
-  //     }, 1000);
-
-  //     //This return acts as a unmount state
-  //     return () => {
-  //       console.log("Images unmount");
-  //       clearInterval(interval);
-  //     };
-  //   }, []);
-  //! ------------------------------------------------------------
-
-  //TODO creating components inside components -------------------
-  //creating a component inside a component to show images
-  // Unique keys, for each element of an array as if the program cant identify each element, how could you update, delete,etc said element, so we use an index of the array for instance here, to give entity to each image. //*BUT indexes are not recommended to use as keys
+  
   function ShowImage(params) {
     return images.map((img, index) => (
       <Image
@@ -146,26 +60,11 @@ export default function images() {
     setnewImageUrl(event.target.value);
     //console.log(event.target.value);
   }
-  //TODO end of components ---------------------------------------------
-
-  //?creating a state to show or not show an elmeent on mouse hover--
-  //unused functions below, just an example of application
-  //   function handleMouseEnter() {
-  //     //console.log("mouse over image");
-  //   }
-  //   function handleMouseLeave() {
-  //     console.log("mouse left the image");
-  //   }
-  //   function crossClass() {
-  //     return;
-  //   }
-  //? ---------------------------------------------------------------
 
   return (
     <section>
-      {/* The hook variable goes here, scrollPosition*/}
       {scrollPosition}
-      {/* <h2>Component is updated {varRef.current} times</h2> */}
+
       <div className="gap-0" style={{
         columnCount:5
       }}>
@@ -197,36 +96,3 @@ export default function images() {
     </section>
   );
 }
-
-//! Class based component down below-------------------------------
-// export default class Images extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { interval: null };
-//   }
-
-//   componentDidMount() {
-//     console.log("Image mounted");
-//     this.setState({
-//       interval: setInterval(() => {
-//         console.log("Hello");
-//       }, 1000),
-//     });
-//   }
-
-//   //Once the component is unmounted, you can perform certain tasks, to make sure you close it properly
-//   componentWillUnmount() {
-//     console.log("Images unmounted");
-//     clearInterval(this.state.interval);
-//   }
-//   render() {
-//     return (
-//       <img
-//         className="mt-2"
-//         src="https://images.unsplash.com/photo-1623275563425-82bfaf4599a3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
-//         alt="roses"
-//       ></img>
-//     );
-//   }
-// }
-//!---------------------------------------------------------------------
