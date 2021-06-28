5a10853716722d6a79aa6385b4ab0d11c91138e7
Diego M <77156529+dmoreira317@users.noreply.github.com>
2021-06-28 11:33:39 -0300

    cleared the code on useFetchImage

--- image-gallery/src/utils/hooks/useFetchImage.js
+++ image-gallery/src/utils/hooks/useFetchImage.js
@@ -10,60 +10,50 @@ function useFetchImage(page, searchTerm) {
     const [errors, seterrors] = useState([])
     const [isLoading, setisLoading] = useState(false)
 
-    function fetchSearch(params) {
+    function fetch(){
+        const url = searchTerm === null ? 'photos?': `search/photos?query=${searchTerm}&`;
+
         axios.get(
-            `${api}/search/photos?client_id=${secret}&page=${page}&query=${searchTerm}`
-          )
-          .then((res) => {
-              if (page > 1){
-                setimages([...images, ...res.data.results]);
-              } else {
-                setimages([...res.data.results])
-              }         
-            setisLoading(false);
-          }).catch(e => {
-              seterrors("Unable to fetch photos");
-              setisLoading(false)
-          })
+            `${api}/${url}client_id=${secret}&page=${page}`
+          ).then((res) => {
+
+            if (searchTerm === null){
+                fetchRandom(res)
+            } else {
+                fetchSearch(res)
+            }
+          setisLoading(false)})
+          .catch(e => {
+            seterrors("Unable to fetch photos");
+            setisLoading(false)
+        })
     }
 
-    function fetchRandom(params) {
-        axios.get(
-            `${api}/photos?client_id=${secret}&page=${page}`
-          )
-          .then((res) => {
-            
-                  setimages([...images,...res.data]);
-      
-            setisLoading(false);
-          }).catch(e => {
-              seterrors("Unable to fetch photos");
-              setisLoading(false)
-          });
+    function fetchSearch(res) {
+        if (page > 1){
+        setimages([...images, ...res.data.results]);
+        } else {
+        setimages([...res.data.results])
+        }         
+    }
+
+    function fetchRandom(res) {       
+        setimages([...images,...res.data]);
     }
     
     useEffect(() => {
-        
         setisLoading(true);
-        
-        if (searchTerm !== null) {
-            fetchSearch();
-        } else {
-            fetchRandom();
-        }
-        
+        fetch()
     }, [page]) //Whenever there's a change in the page variable, the brackets work
 
     useEffect(() => {
         if (searchTerm === null) return;
         setisLoading(true)
-        
-        fetchSearch();
-
+        fetch();
     }, [searchTerm])
     
 
-      return [images, setimages, errors, isLoading];
+    return [images, setimages, errors, isLoading];
 }
 
 export default useFetchImage
