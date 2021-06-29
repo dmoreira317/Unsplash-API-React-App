15dc95490776138f86ea3e44db96e907d1000e73
Diego M <77156529+dmoreira317@users.noreply.github.com>
2021-06-29 14:21:51 -0300

    Handled submit to take in new users in firebase

--- image-gallery/src/page/SignUp.js
+++ image-gallery/src/page/SignUp.js
@@ -1,53 +1,53 @@
 import React, {useState} from 'react'
 import firebase from '../config/firebase'
 import { useHistory } from "react-router-dom";
-import { Formik, useFormik, useFormikContext } from 'formik';
+import { Field, ErrorMessage, Form, Formik, useFormik, useFormikContext } from 'formik';
 import * as Yup from 'yup'
 
 
 export default function SignUp(){
-    const formik = useFormik({
-        initialValues: {email:"", password:""},
-        onSubmit: (value) => {
-            console.log("Formik", value);
-        },
-        validationSchema: Yup.object({
+    const history = useHistory()
+    return (
+        <Formik 
+        initialValues={{email:"", password:""}}
+        onSubmit={(value, formikBag) => {
+            firebase.auth().createUserWithEmailAndPassword(value.email, value.password).then(
+                (res) =>{
+                    history.push('/')
+                }
+            ).catch((e)=>{
+                formikBag.setFieldError('email', e.message)
+            });
+        }}
+        validationSchema={Yup.object({
             email: Yup.string().required('Email is required.').email('Email is invalid.'),
             password: Yup.string().required('Password is required.').min(6)
-        }),
-    })
-
-    return (
+        })}>
+        
         <div className="flex h-screen bg-gray-200">
             <div className="m-auto text-3xl flex flex-wrap justify-center shadow-lg rounded-lg bg-gradient-to-br from-indigo-900 to-indigo-700">
-                <form className="m-5 w-10/12" onSubmit={formik.handleSubmit}>
+                <Form className="m-5 w-10/12">
                     <h1 className="w-full text-4xl tracking-widest text-center my-6 text-white">
                         Sign Up
                     </h1>
                     <div className="w-full my-6">
-                        <input type="email" className="p-2 rounded shadow w-full" placeholder="Email or username" name="email" value={formik.values.email} 
-                        onChange={formik.handleChange} onBlur={formik.handleBlur}/>
-                        {
-                            formik.touched.email && formik.errors.email ? <p>{formik.errors.email}</p>: null
-                        }
+                        <Field type="email" className="p-2 rounded shadow w-full" placeholder="Email or username" name="email"/>
+                         <ErrorMessage name="email" />
                         
                     </div>
                     <div className="w-full my-6">
-                        <input type="password" className="p-2 rounded shadow w-full" placeholder="Password" name="password" value={formik.values.password} onChange={formik.handleChange} onBlur={formik.handleBlur}/>
-                        {
-                            formik.touched.password && formik.errors.password ? <p>{formik.errors.password}</p>: null
-                        }
+                        <Field type="password" name="password" className="p-2 rounded shadow w-full" placeholder="Password"/>
+                        <ErrorMessage name="password" />
+
                     </div>
                     <div className="w-full my-10">
                         <button type="submit" className="p-2 rounded shadow w-full bg-gradient-to-tr from-yellow-600 to-yellow-400 text-black">
                             Sign Up
                         </button>
                     </div>
-                   
-                </form>
-                
+                </Form>
             </div>
-            
       </div>
-    )
-}
+    </Formik>
+    
+)}
