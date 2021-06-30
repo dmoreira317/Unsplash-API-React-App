c88b3ba046f15eb3cc6d4c2f18df63566d1f0cee
Diego M <77156529+dmoreira317@users.noreply.github.com>
2021-06-30 11:44:17 -0300

    Modified tensorflow page to show predictions on button

--- image-gallery/src/page/TensorFlow.js
+++ image-gallery/src/page/TensorFlow.js
@@ -1,28 +1,35 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import '@tensorflow/tfjs'
 import * as mobilenet from '@tensorflow-models/mobilenet'
 
 export default function TensorFlow() {
-    const imageRef = useRef()
+    const imageRef = useRef();
+    const [isLoading, setisLoading] = useState(false)
 
     function predict(){
         const img = imageRef.current;
+        setisLoading(true);
         mobilenet.load().then(model => {
             // Classify the image.
             model.classify(img).then(predictions => {
                 console.log('Predictions: ');
                 console.log(predictions);
+                setisLoading(false);
             });
             });
     }
 
     return (
-        <div className="flex justify-center">
+        <div className="flex justify-center text-center">
             <div className="w-1/3">
                 <h1>TensorFlow Example</h1>
-                <img src="https://images.unsplash.com/photo-1583511655826-05700d52f4d9?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzg5MjR8MHwxfHNlYXJjaHw1fHxkb2d8ZW58MHx8fHwxNjI0OTk0ODEw&ixlib=rb-1.2.1&q=80&w=1080" width='400' crossOrigin="anonymous" ref={imageRef}></img>
-                <div className="text-center m-5"><button className="p-2 rounded bg-gray-900 text-white" onClick={predict}>Predict result</button></div>
-                
+                <img className="text-center" src="https://images.unsplash.com/photo-1583511655826-05700d52f4d9?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzg5MjR8MHwxfHNlYXJjaHw1fHxkb2d8ZW58MHx8fHwxNjI0OTk0ODEw&ixlib=rb-1.2.1&q=80&w=1080" width='400' crossOrigin="anonymous" ref={imageRef}></img>
+                <div className="m-5">
+                    <button className="p-2 rounded bg-gray-900 text-white w-62" onClick={predict}>
+                        {isLoading && '⏳'}   
+                        {!isLoading && 'Predict result'}
+                    </button>
+                </div>
             </div>
         </div>
     )
