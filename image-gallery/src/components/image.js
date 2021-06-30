7a21b37b24005dd18f1800c511538dc5a3b415ec
Diego M <77156529+dmoreira317@users.noreply.github.com>
2021-06-30 10:49:41 -0300

    Added prop-types library to auto generate errors

--- image-gallery/src/components/image.js
+++ image-gallery/src/components/image.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import PropTypes from 'prop-types';
 
 function Image({ handleRemove, image, index, show }) {
   const [isHovering, setisHovering] = useState(false);
@@ -23,21 +24,25 @@ function Image({ handleRemove, image, index, show }) {
   );
 }
 
-const types = {
-  function(props, propName){
-    if (typeof(props[propName]) !== 'function'){
-      return new Error(`'${propName}' must be a function, but received ${typeof props[propName]}`);
-    }
-  },
-  number(props, propName){
-    if (typeof(props[propName]) !== 'number'){
-      return new Error(`'${propName}' must be a number, but received ${typeof props[propName]}`);
-    }
-  },
-}
+//TODO Manually created errors
+// const types = {
+//   function(props, propName){
+//     if (typeof(props[propName]) !== 'function'){
+//       return new Error(`'${propName}' must be a function, but received ${typeof props[propName]}`);
+//     }
+//   },
+//   number(props, propName){
+//     if (typeof(props[propName]) !== 'number'){
+//       return new Error(`'${propName}' must be a number, but received ${typeof props[propName]}`);
+//     }
+//   },
+// }
 
+//importing errors from prop-types library
 Image.propTypes = {
-  show: types.function,
-  index: types.number,
+  show: PropTypes.func,
+  image: PropTypes.string,
+  index: PropTypes.number,
+  handleRemove: PropTypes.func,
 }
 export default Image;
